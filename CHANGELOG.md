cf1b5fc66dcadb305dd61bd180c7aae465794965
guonianlei <guonianlei@sf-express.com>
2024-03-18 15:07:00 +0800

    修改插件描述

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Convert-Bean-JSON Changelog
+
+
+## [1.0.0] - 2024-03-18
+- 1.0.0 插件发布
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
+import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+fun properties(key: String) = project.findProperty(key).toString()
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
     id("org.jetbrains.intellij") version "1.15.0"
+    // Gradle Changelog Plugin
+    id("org.jetbrains.changelog") version "1.3.1"
 }
 
 group = "com.guonl"
@@ -22,6 +29,12 @@ intellij {
     plugins.set(listOf("com.intellij.java"))
 }
 
+// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+    version.set(properties("pluginVersion"))
+    groups.set(emptyList())
+}
+
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
@@ -33,8 +46,30 @@ tasks {
     }
 
     patchPluginXml {
+        version.set(properties("pluginVersion"))
         sinceBuild.set("222")
         untilBuild.set("233.*")
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        pluginDescription.set(
+            projectDir.resolve("README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").run { markdownToHTML(this) }
+        )
+
+        // Get the latest available change notes from the changelog file
+        changeNotes.set(provider {
+            changelog.run {
+                getOrNull(properties("pluginVersion")) ?: getLatest()
+            }.toHTML()
+        })
+
     }
 
     signPlugin {
